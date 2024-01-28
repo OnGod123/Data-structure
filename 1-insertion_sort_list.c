1e3ea1a8aadeeb2e0dbb9cf45ea970e372d16ebe
OnGod123 <ifejikav4.@gmail.com>
2024-01-27 16:38:46 -0800

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,57 +1,47 @@
-#include <stdio.h>
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- * @list: Pointer to the head of the doubly linked list
+ * insertion_sort_list - sorts a DLL of integers in
+ * ascending order using the insertion sort
+ * algorithm
+ *
+ * @list: doubly linked list
+ * Return: no return
  */
 void insertion_sort_list(listint_t **list)
 {
-   	 if (list == NULL || *list == NULL)
+	listint_t *ptr, *tmp;
+
+	if (!list)
+		return;
+
+	ptr = *list;
+
+	while (ptr)
 	{
-        	return;
+		while (ptr->next && (ptr->n > ptr->next->n))
+		{
+			tmp = ptr->next;
+			ptr->next = tmp->next;
+			tmp->prev = ptr->prev;
+
+			if (ptr->prev)
+				ptr->prev->next = tmp;
+
+			if (tmp->next)
+				tmp->next->prev = ptr;
+
+			ptr->prev = tmp;
+			tmp->next = ptr;
+
+			if (tmp->prev)
+				ptr = tmp->prev;
+			else
+				*list = tmp;
+
+			print_list(*list);
+		}
+		ptr = ptr->next;
 	}
-    
-    listint_t *current;
-    listint_t *next;
-    listint_t *tempNext;
-   listint_t *tempPrev;
-
-    
-    current = *list;
-    next = NULL;
-
-    while (current != NULL)
-    {
-        next = current->next;
-
-        while (current->prev != NULL && current->n < current->prev->n)
-        {
-            tempNext = current->next;
-            tempPrev = current->prev;
-
-            if (tempNext != NULL)
-            {
-                tempNext->prev = tempPrev;
-                current->next = tempPrev;
-                current->prev = tempPrev->prev;
-                current->next->prev = current;
-            }
-            else
-            {
-                tempPrev->next = current;
-                current->prev = tempPrev->prev;
-                current->next = tempPrev;
-                tempPrev->prev = current;
-
-                *list = current;
-            }
-
-            
-            print_list(*list);
-        }
-
-        current = next;
-    }
 }
-
