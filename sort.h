9e75bf51696650c5352e4e182f57f36716e94c17
OnGod123 <ifejikav4.@gmail.com>
2024-01-27 17:34:34 -0800

    Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -22,4 +22,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void _qsort(int *a, int low, int high, int size);
 void print_list(const listint_t *list);
+void shell_sort(int *array, size_t size);
+void _ssort(int *a, int size, int n);
 #endif
